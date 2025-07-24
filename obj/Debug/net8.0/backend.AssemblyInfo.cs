421591ed365c58b84d5087d877e172dcc1b3f8d7
Caiuã Mello <105950039+caiua0800@users.noreply.github.com>
2025-07-24 14:59:20 -0300

    faz parte

--- obj/Debug/net8.0/backend.AssemblyInfo.cs
+++ obj/Debug/net8.0/backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9fed4886eabad2aa4eb771d034b9f15f58b54a1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0b521bf9cab16dfccaf3c82edcca68c99235a25b")]
 [assembly: System.Reflection.AssemblyProductAttribute("backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
